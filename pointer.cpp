157f7af0d8bf650ed690cbc39b48c8149631b179
Pranta Saha <pranta1204@gmail.com>
2024-04-02 21:03:24 +0600

    added null pointer

--- pointer.cpp
+++ pointer.cpp
@@ -4,7 +4,7 @@ int main()
 {
     // & address-of operator
     // * dereference operator
-    
+
     std::string name = "Pranta";
     int age = 30;
     std::string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
@@ -16,7 +16,22 @@ int main()
     std::cout << pName << std::endl;
     std::cout << pAge << std::endl;
     std::cout << pFreePizzas << std::endl;
-    std::cout << *pName << std::endl;
+
+    // Null pointer
+    // 'nullptr' keyword represents a null pointer literal
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+    // null pointer is not safe dereference without address assignment
+    if (pointer == nullptr)
+    {
+        std::cout << "address was not assigned";
+    }
+    else
+    {
+        std::cout << "address was assigned";
+    }
 
     return 0;
 }
