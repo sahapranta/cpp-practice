5c125661d596e0dec34f7c57f0371dfe5121f32c
Pranta Saha <pranta1204@gmail.com>
2024-04-02 18:44:14 +0600

    Create typedef.cpp

---
+++ typedef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+// typedef  std::vector<std::pair<std::string, int>> pairList_t;
+// typedef std::string text_t;
+
+using text_t = std::string;
+
+int main()
+{
+    // pairList_t list
+    text_t firstName = "Pranta";
+
+    std::cout << firstName << std::endl;
+
+    return 0;
+}
